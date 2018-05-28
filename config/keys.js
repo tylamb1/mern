4cd0e2a0b03a4625128fbd30f845014a4c972bef
Ty Lamb <ty@tylamb.com>
2018-05-28 00:03:02 -0500

    After lecture 15

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,5 @@
 module.exports = {
-  mongoURI: 'mongodb://mern:mernmernmern@ds229690.mlab.com:29690/mern'
+  mongoURI: 'mongodb://mern:mernmernmern@ds229690.mlab.com:29690/mern',
+
+  secretOrKey: 'HQBcf4LvChQ52bbcs2uYgjGvJrtcZmiX0ZHKR3oxdZvg2Gj'
 };
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,12 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
+const passport = require('passport');
+
+// Load Input Validation
+const validateRegisterInput = require('../../validation/register');
 
 // load User model
 const User = require('../../models/User');
@@ -17,11 +23,18 @@ router.get('/test', (req, res) => {
 // @desc    Register a user
 // @access  Public
 router.post('/register', (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  // Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
   // does email exist
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
+      errors.email = 'Email already exists';
       // user already exist
-      return res.status(400).json({ email: 'Email already exists' });
+      return res.status(400).json({ errors });
     } else {
       // email is available
 
@@ -50,6 +63,60 @@ router.post('/register', (req, res) => {
         });
       });
     }
+  });
+});
+
+// @route   GET api/users/login
+// @desc    Login User / Returning JWT Token
+// @access  Public
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // find the user by email
+  User.findOne({ email }).then(user => {
+    // check for user
+
+    if (!user) {
+      // no user found
+      return res.status(404).json({ email: 'User not found' });
+    }
+
+    // check password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        // password matches
+        //res.json({ msg: 'Success' });
+
+        const payload = { id: user.id, name: user.name, avatar: user.avatar }; // Create JWT Payload
+
+        // Sign Token
+        jwt.sign(payload, keys.secretOrKey, { expiresIn: 3600 }, (err, token) => {
+          res.json({
+            sucess: true,
+            token: 'Bearer ' + token
+          });
+          // callback for the jwt sign
+        });
+      } else {
+        // password failed
+        return res.status(400).json({ password: 'Password incorrect' });
+      }
+    });
+  });
+});
+
+// @route   GET api/users/current
+// @desc    Return the current user
+// @access  Public
+router.get('/current', passport.authenticate('jwt', { session: false }), (req, res) => {
+  //
+
+  //res.json({ msg: 'Success' });
+  res.json({
+    id: req.user.id,
+    name: req.user.name,
+    email: req.user.email
   });
 });
 
